fab4230d268819a4762439332189e523b134a2c5
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2024-08-21 12:05:39 +0530

    update

--- app/Http/Controllers/CreateController.php
+++ app/Http/Controllers/CreateController.php
@@ -171,7 +171,7 @@ class CreateController extends Controller
 
     public function logout(Request $request)
     {
-        dd("abc");
+        dd($request->user());
         // Check if the user is authenticated
         if (!$request->user()) {
             return response()->json([
