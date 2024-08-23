a8f1c21c12a4b2947e1524326d3fcc83b790f86a
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2024-08-23 14:13:29 +0530

    enable authentication

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use App\Http\Middleware\GetUserRole;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-// Route::prefix('admin')->middleware(['auth:sanctum', GetUserRole::class . ':admin'])->group(function () {
+Route::prefix('admin')->middleware(['auth:sanctum', GetUserRole::class . ':admin'])->group(function () {
 
     Route::post('/add_user', [CreateController::class, 'user']);
 
@@ -69,9 +69,9 @@ use App\Http\Middleware\GetUserRole;
     Route::post('/add_counter', [CreateController::class, 'counter']);
 
     Route::get('/view_counter', [ViewController::class, 'counter']);
-// });
+});
 
-// Route::prefix('user')->middleware(['auth:sanctum', GetUserRole::class . ':user'])->group(function () {
+Route::prefix('user')->middleware(['auth:sanctum', GetUserRole::class . ':user'])->group(function () {
 
     Route::get('/get_details', [ViewController::class, 'user_details']);
 
@@ -86,7 +86,7 @@ use App\Http\Middleware\GetUserRole;
 
     Route::post('/add_order', [CreateController::class, 'orders']);
 
-// });
+});
 Route::post('/login/{otp?}', [CreateController::class, 'login']);
 
 Route::post('/register_user', [CreateController::class, 'user']);
