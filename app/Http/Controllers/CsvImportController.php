2885d0409893236708608280ff43862d7577251c
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2024-09-10 16:03:34 +0530

    update for csv import controller

--- app/Http/Controllers/CsvImportController.php
+++ app/Http/Controllers/CsvImportController.php
@@ -37,6 +37,9 @@ class CsvImportController extends Controller
 
         $records_csv = (new Statement())->process($csv_product);
 
+        $product_insert_response = null;
+        $product_update_response = null;
+
         // Iterate through each record and create or update the product
         foreach ($records_csv as $record_csv) {
             $product_csv = ProductModel::where('sku', $record_csv['SKU'])->first();
@@ -105,7 +108,7 @@ class CsvImportController extends Controller
             if ($product_csv) 
             {
                 // If product exists, update it
-                $product_csv->update([
+                $product_update_response = $product_csv->update([
                     'product_code' => $record_csv['Product Code'],
                     'product_name' => $record_csv['Product Name'],
                     'category' => $record_csv['Category'],
@@ -119,7 +122,7 @@ class CsvImportController extends Controller
             else 
             {
                 // If product does not exist, create a new one
-                ProductModel::create([
+                $product_insert_response = ProductModel::create([
                     'sku' => $record_csv['SKU'],
                     'product_code' => $record_csv['Product Code'],
                     'product_name' => $record_csv['Product Name'],
@@ -132,7 +135,12 @@ class CsvImportController extends Controller
                 ]);
             }
         }   
-        return response()->json(['message' => 'Products imported successfully'], 200);
+        if ($product_update_response == 1 || isset($product_insert_response)) {
+            return response()->json(['message' => 'Products imported successfully'], 200);
+        }
+        else {
+            return response()->json(['message' => 'Sorry, failed to imported successfully'], 400);
+        }
     }
 
     public function importUser()
@@ -151,6 +159,9 @@ class CsvImportController extends Controller
 
         $records_user = (new Statement())->process($csv_user);
 
+        $get_insert_response = null;
+        $get_update_response = null;
+
         // Iterate through each record and create or update the product
         foreach ($records_user as $record_user) {
             $user_csv = User::where('mobile', $record_user['Mobile '])->first();
@@ -163,7 +174,7 @@ class CsvImportController extends Controller
             if ($user_csv) 
             {
                 // If user exists, update it
-                $user_csv->update([
+                $get_update_response = $user_csv->update([
                     'name' => $record_user['Name'],
                     'email' => $email_user,
                     'password' => bcrypt($record_user['Mobile ']),
@@ -182,7 +193,7 @@ class CsvImportController extends Controller
             else 
             {
                 // If user does not exist, create a new one
-                User::create([
+                $get_insert_response = User::create([
                     'mobile' => $record_user['Mobile '],
                     'name' => $record_user['Name'],
                     'email' => $email_user,
@@ -200,6 +211,12 @@ class CsvImportController extends Controller
                 ]);
             }
         }   
-        return response()->json(['message' => 'Users imported successfully'], 200);
+
+        if ($get_update_response == 1 || isset($get_insert_response)) {
+            return response()->json(['message' => 'Users imported successfully'], 200);
+        }
+        else {
+            return response()->json(['message' => 'Sorry, failed to imported successfully'], 400);
+        }
     }
 }
