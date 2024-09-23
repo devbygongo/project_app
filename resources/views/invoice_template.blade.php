05b818fa649bd24ae386b8df63ca31bb1a64872e
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2024-09-23 17:35:35 +0530

    update as per requirement

--- resources/views/invoice_template.blade.php
+++ resources/views/invoice_template.blade.php
@@ -119,7 +119,7 @@
             @foreach($invoice_items as $index => $item)
                 <tr>
                     <td class="center-align">{{ $index + 1 }}</td>
-                    <td><img src="{{ Storage::url('uploads/products/' . $item->product_code . '.jpg') }}" alt="" style="height: 60px; width: 60px;"></td>
+                    <td><img src="{{ Storage::url('uploads/products_pdf/' . $item->product_code . '.jpg') }}" alt="" style="height: 60px; width: 60px;"></td>
                     <td>{{ $item->product_name }}<br>Part No: {{ $item->product_code }}</td>
                     <td class="center-align">{{ $item->quantity }}</td>
                     <td class="right-align">₹ {{ $item->rate }}</td>
