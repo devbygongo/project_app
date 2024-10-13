86879d77d9cf09ce18e3ce25a6dd764fd1d2191b
Burhanuddin Hatim Kanchwala <dotcomsolutiononline@gmail.com>
2024-10-13 15:51:33 +0530

    ..

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -165,7 +165,7 @@ class InvoiceController extends Controller
 
         foreach ($mobileNumbers as $mobileNumber) 
         {
-            if($mobileNumber == '+918961043773')
+            if($mobileNumber != '+918961043773')
             {
                 // Send message for each number
                 $response = $whatsAppUtility->sendWhatsApp($mobileNumber, $templateParams, '', 'Admin Order Invoice');
