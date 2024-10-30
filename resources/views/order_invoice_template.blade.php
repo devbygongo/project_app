f1aad5a1b629674e0977211ce5b592c17e7d04dd
Burhanuddin Hatim Kanchwala <dotcomsolutiononline@gmail.com>
2024-10-30 21:34:24 +0530

    ..

--- resources/views/order_invoice_template.blade.php
+++ resources/views/order_invoice_template.blade.php
@@ -124,7 +124,7 @@
                 <tr>
                     <td class="center-align">{{ $index + 1 }}</td>
                     <td><img src="{{ Storage::url('uploads/products_pdf/' . $item->product_code . '.jpg') }}" alt="" style="height: 60px; width: 60px;"></td>
-                    <td>{{ $item->product_name }}<br>Part No: {{ $item->product->product_code }}<br>{{ $item->product->remarks }}</td>
+                    <td>{{ $item->product_name }}<br>Part No: {{ $item->product->product_code }}<br>{{ $item->remarks }}</td>
                     <td class="center-align">{{ $item->quantity }}</td>
                     <td class="right-align">₹ {{ number_format((float)$item->rate, 2) }}</td>
                     <td class="right-align">₹ {{ number_format((float)$item->total, 2) }}</td>
