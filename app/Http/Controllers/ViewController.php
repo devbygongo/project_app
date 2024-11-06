70237041a098e533bf8b63c88715fd971b476948
Burhanuddin Hatim Kanchwala <dotcomsolutiononline@gmail.com>
2024-11-06 11:53:22 +0530

    ..

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -122,9 +122,24 @@ class ViewController extends Controller
 				'sub_category', 
 				'product_image', 
 				DB::raw('special_basic as basic'), 
-				DB::raw('special_gst as gst')
+				DB::raw('special_gst as gst'),
+                'out_of_stock',
+                'yet_to_launch'
 			);
-		} else {
+		}  else if ($user_type && $user_type->type == 'outstation') {
+            // If user type is 'special', select special columns but alias them as 'basic' and 'gst'
+            $query = ProductModel::select(
+				'product_code', 
+				'product_name', 
+				'category', 
+				'sub_category', 
+				'product_image', 
+				DB::raw('outstation_basic as basic'), 
+				DB::raw('outstation_gst as gst'),
+                'out_of_stock',
+                'yet_to_launch'
+			);
+        } else {
 			// Default columns for non-special users
 			$query = ProductModel::select(
 				'product_code', 
