b68c1ee00cd8fe0d7295e327b1f212f31013e9a6
Burhanuddin Hatim Kanchwala <dotcomsolutiononline@gmail.com>
2024-11-06 21:56:49 +0530

    Add files via upload

---
+++ resources/views/order_invoice_template_footer.blade.php
@@ -0,0 +1,13 @@
+        <tr>
+            <td colspan="5" class="right-align">Total</td>
+            <td class="right-align">₹ {{ number_format((float)$order->amount, 2) }}</td>
+        </tr>
+    </tbody>
+</table>
+
+<div class="footer">
+    <p>Thank you for working with us</p>
+</div>
+
+</body>
+</html>
