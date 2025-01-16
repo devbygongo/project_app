f03588b7b039efcb97ed3ef9abaa4fa14ac34fa4
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-01-16 17:33:10 +0530

    fix for godown id

--- app/Models/StockOrderItemsModel.php
+++ app/Models/StockOrderItemsModel.php
@@ -14,6 +14,7 @@ class StockOrderItemsModel extends Model
         'stock_order_id',
         'product_code',
         'product_name',
+        'godown_id',
         'quantity',
         'type',
     ];
