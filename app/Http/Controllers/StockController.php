7c5a8179537617eafa79bda54f8cf1059178362a
Burhanuddin Hatim Kanchwala <dotcomsolutiononline@gmail.com>
2025-02-06 12:38:29 +0530

    Modified Stock Report

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -3,10 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use Mpdf\Mpdf;
 use App\Models\StockOrderItemsModel;
-use App\Models\ProductModel;
 use App\Models\GodownModel;
 use Illuminate\Support\Facades\File;
 use Carbon\Carbon;
@@ -15,58 +13,94 @@ class StockController extends Controller
 {
     public function generateStockReport()
     {
-        // Fetch stock data from the `t_stock_order_items` table
+        // Fetch stock data from `t_stock_order_items`
         $stockData = StockOrderItemsModel::with('stock_product', 'godown')->get();
 
-        // Calculate current stock for each product
+        // Organize stock by product and godown
         $stockSummary = [];
         foreach ($stockData as $item) {
             $productCode = $item->product_code;
-            
+            $godown = $item->godown->name ?? 'N/A';
+
             if (!isset($stockSummary[$productCode])) {
                 $stockSummary[$productCode] = [
                     'product_code' => $item->product_code,
                     'product_name' => $item->product_name,
-                    'godown' => $item->godown->name ?? 'N/A',
-                    'quantity' => 0,
+                    'godowns' => [],
+                    'total_stock' => 0
                 ];
             }
 
-            // Add or subtract stock based on type (IN or OUT)
+            // Initialize godown stock if not set
+            if (!isset($stockSummary[$productCode]['godowns'][$godown])) {
+                $stockSummary[$productCode]['godowns'][$godown] = 0;
+            }
+
+            // Update stock based on type (IN or OUT)
             if ($item->type === 'IN') {
-                $stockSummary[$productCode]['quantity'] += $item->quantity;
+                $stockSummary[$productCode]['godowns'][$godown] += $item->quantity;
+                $stockSummary[$productCode]['total_stock'] += $item->quantity;
             } elseif ($item->type === 'OUT') {
-                $stockSummary[$productCode]['quantity'] -= $item->quantity;
+                $stockSummary[$productCode]['godowns'][$godown] -= $item->quantity;
+                $stockSummary[$productCode]['total_stock'] -= $item->quantity;
             }
         }
 
+        // Fetch all Godown Names dynamically
+        $allGodowns = GodownModel::pluck('name')->toArray();
+
         // Generate PDF
-        $mpdf = new Mpdf();
+        $mpdf = new Mpdf([
+            'mode' => 'utf-8',
+            'format' => 'A4-L', // Landscape for better table display
+            'margin_top' => 25, // Adjusted for header spacing
+        ]);
+
         $mpdf->SetTitle("Stock Report");
 
-        // Define the HTML content
-        $html = '<h2 style="text-align:center;">Stock Report</h2>';
-        $html .= '<p>Date: ' . Carbon::now()->format('d-m-Y H:i') . '</p>';
-        $html .= '<table border="1" width="100%" cellpadding="5" cellspacing="0">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Product Code</th>
-                            <th>Product Name</th>
-                            <th>Godown</th>
-                            <th>Stock Quantity</th>
+        // HTML Header (Repeated on Each Page)
+        $headerHtml = '<table border="1" width="100%" cellpadding="5" cellspacing="0" style="border-collapse: collapse; font-size: 12px;">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Part No</th>
+                                <th>Product Name</th>';
+        foreach ($allGodowns as $godown) {
+            $headerHtml .= "<th>{$godown}</th>";
+        }
+        $headerHtml .= '<th>Total Stock</th>
                         </tr>
                     </thead>
+                </table>';
+
+        $mpdf->SetHTMLHeader($headerHtml);
+
+        // HTML Content
+        $html = '<div style="text-align:center;">
+                    <h2>Stock Report</h2>
+                    <p>Date: ' . Carbon::now()->format('d-m-Y H:i') . '</p>
+                 </div>';
+
+        // Start table
+        $html .= '<table border="1" width="100%" cellpadding="5" cellspacing="0" style="border-collapse: collapse; font-size: 12px;">
+                    <thead>' . $headerHtml . '</thead>
                     <tbody>';
-        
+
+        // Populate table rows
         $index = 1;
-        foreach ($stockSummary as $stock) {
+        foreach ($stockSummary as $product) {
             $html .= "<tr>
                         <td>{$index}</td>
-                        <td>{$stock['product_code']}</td>
-                        <td>{$stock['product_name']}</td>
-                        <td>{$stock['godown']}</td>
-                        <td>{$stock['quantity']}</td>
+                        <td>{$product['product_code']}</td>
+                        <td>{$product['product_name']}</td>";
+
+            // Fill stock for each godown
+            foreach ($allGodowns as $godown) {
+                $stockInGodown = $product['godowns'][$godown] ?? 0;
+                $html .= "<td>{$stockInGodown}</td>";
+            }
+
+            $html .= "<td>{$product['total_stock']}</td>
                     </tr>";
             $index++;
         }
@@ -90,12 +124,8 @@ class StockController extends Controller
 
         // Return download link
         return response()->json([
-            asset('storage/reports/' . $fileName),
+            'download_url' => asset('storage/reports/' . $fileName),
         ]);
-
-        // return response()->download(asset('storage/reports/' . $fileName));
-
     }
 }
-
 ?>
