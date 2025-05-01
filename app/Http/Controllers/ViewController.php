021538f0ceff0780fc82a0b9c01d415c16bf50db
Burhanuddin Hatim Kanchwala <dotcomsolutiononline@gmail.com>
2025-05-01 21:52:00 +0530

    ..

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -961,7 +961,7 @@ class ViewController extends Controller
 
         // Modify the order items to append the product image directly
         $get_user_orders->each(function($order) {
-            if ($get_user->mobile = "+919951263651") {
+            if ($get_user->mobile == "+919951263651") {
                 $order->amount = 0;
             }
             $order->order_items->each(function($orderItem) {
