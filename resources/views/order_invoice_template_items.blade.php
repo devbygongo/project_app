577928be4cafa291ed6d4980f42e2f9cd1f1c6e2
Burhanuddin Hatim Kanchwala <dotcomsolutiononline@gmail.com>
2025-06-02 21:32:32 +0530

    ..

--- resources/views/order_invoice_template_items.blade.php
+++ resources/views/order_invoice_template_items.blade.php
@@ -1,7 +1,7 @@
 <tr>
     <td class="center-align">{{ $index + 1 }}</td>
     <td><img src="{{ Storage::url('uploads/products_pdf/' . $item->product_code . '.jpg') }}" alt="Product Image" style="height: 60px; width: 60px;"></td>
-    <td>{{ $item->product_name }}<br>Part No: {{ $item->product->product_code }}<br><span style="background: yellow;">Remarks : {{ $item->remarks }}</span></td>
+    <td>{{ $item->product_name }}<br>Part No: {{ $item->product->product_code }}<br><span style="background: yellow;">{{ $item->remarks }}</span></td>
     <td class="center-align">{{ $item->quantity }}</td>
     <td class="right-align">₹ {{ number_format((float)$item->rate, 2) }}</td>
     <td class="right-align">₹ {{ number_format((float)$item->total, 2) }}</td>
--- resources/views/packing_slip_template_items.blade.php
+++ resources/views/packing_slip_template_items.blade.php
@@ -1,6 +1,6 @@
 <tr>
     <td class="center-align">{{ $index + 1 }}</td>
     <td><img src="{{ Storage::url('uploads/products_pdf/' . $item->product_code . '.jpg') }}" alt="Product Image" style="height: 60px; width: 60px;"></td>
-    <td>{{ $item->product_name }}<br>Part No: {{ $item->product->product_code }}<br><span style="background: yellow;">Remarks : {{ $item->remarks }}</span></td>
+    <td>{{ $item->product_name }}<br>Part No: {{ $item->product->product_code }}<br><span style="background: yellow;">{{ $item->remarks }}</span></td>
     <td class="center-align">{{ $item->quantity }}</td>
 </tr>
