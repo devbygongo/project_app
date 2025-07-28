aa2cf2202bd135e55e3179f45865ea5852c1ee8c
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-07-28 13:35:44 +0530

    update as per design requirement

--- resources/views/packing_slip_template_header.blade.php
+++ resources/views/packing_slip_template_header.blade.php
@@ -49,8 +49,8 @@
             <th class="center-align">SN</th>
             <th>Photo</th>
             <th>Product Name</th>
-            <th class="center-align">Current Stock</th>
             <th class="center-align">Qty</th>
+            <th class="center-align">Current Stock</th>
         </tr>
     </thead>
     <tbody>
--- resources/views/packing_slip_template_items.blade.php
+++ resources/views/packing_slip_template_items.blade.php
@@ -2,6 +2,6 @@
     <td class="center-align">{{ $index + 1 }}</td>
     <td><img src="{{ Storage::url('uploads/products_pdf/' . $item->product_code . '.jpg') }}" alt="Product Image" style="height: 60px; width: 60px;"></td>
     <td>{{ $item->product_name }}<br>Part No: {{ $item->product->product_code }}<br><span style="background: yellow;">{{ $item->remarks }}</span></td>
-    <td class="center-align">{{ $item->current_stock }}</td>
     <td class="center-align">{{ $item->quantity }}</td>
+    <td class="center-align">{{ $item->current_stock }}</td>
 </tr>
