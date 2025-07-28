59668e102eeb87faea49d76ab6f1227d80c2dcf0
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-07-28 15:15:37 +0530

    test

--- resources/views/packing_slip_template_items.blade.php
+++ resources/views/packing_slip_template_items.blade.php
@@ -1,7 +1,16 @@
 <tr>
-    <td class="center-align">{{ $index + 1 }}</td>
-    <td><img src="{{ Storage::url('uploads/products_pdf/' . $item->product_code . '.jpg') }}" alt="Product Image" style="height: 60px; width: 60px;"></td>
-    <td>{{ $item->product_name }}<br>Part No: {{ $item->product->product_code }}<br><span style="background: yellow;">{{ $item->remarks }}</span></td>
-    <td class="center-align">{{ $item->quantity }}</td>
-    <td class="center-align stock-cell">{{ $item->current_stock }}</td>
+  <td class="center-align">{{ $index + 1 }}</td>
+  <td>
+    <img
+      src="{{ Storage::url('uploads/products_pdf/' . $item->product_code . '.jpg') }}"
+      alt="Product Image"
+      style="height: 60px; width: 60px"
+    />
+  </td>
+  <td>
+    {{ $item->product_name }}<br />Part No: {{ $item->product->product_code
+    }}<br /><span style="background: yellow">{{ $item->remarks }}</span>
+  </td>
+  <td class="center-align">{{ $item->quantity }}</td>
+  <td class="center-align stock-cell">{{ $item->current_stock }}</td>
 </tr>
