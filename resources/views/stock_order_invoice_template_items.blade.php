2688e1f231a3538cb9e78e6bcfa162d20741456f
Burhanuddin Hatim Kanchwala <dotcomsolutiononline@gmail.com>
2025-08-12 21:37:04 +0530

    ..

--- resources/views/stock_order_invoice_template_items.blade.php
+++ resources/views/stock_order_invoice_template_items.blade.php
@@ -3,7 +3,14 @@
     <td><img src="{{ url($stock_item->stock_product->product_image) }}" 
              alt="Product Image" 
              style="height: 60px; width: 60px;"></td>
-    <td>{{ $stock_item->product_name }}<br>Part No: {{ $stock_item->product_code }}<br>{{ $stock_item->remarks }}</td>
+    <td>{{ $stock_item->product_name }}<br>Part No: {{ $stock_item->product_code }}
+    
+    @if(! empty($stock_item->size))
+        <span style="background: yellow;">
+            Size: {{ $stock_item->size }}
+        </span><br>
+    @endif
+    <br>{{ $stock_item->remarks }}</td>
     <td class="center-align">{{ $godown_name ?? 'N/A' }}</td>
     <td class="center-align">{{ $stock_item->quantity }}</td>
     <td class="center-align">{{ strtoupper($stock_item->type) }}</td>
