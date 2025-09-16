b4b381c6e037fa69e7ecd9a8017620f96ee0f1fe
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-09-16 17:36:15 +0530

    fix

--- app/Http/Controllers/CreateController.php
+++ app/Http/Controllers/CreateController.php
@@ -1115,8 +1115,8 @@ class CreateController extends Controller
         try {
             // Validate input
             $validated = $request->validate([
-                'user_id'      => 'required|integer|exists:users,hasidhed_id',
-                'product_code' => 'required|integer|exists:t_products,hasidhed_id',
+                'user_id'      => 'required|integer|exists:users,id',
+                'product_code' => 'required|integer|exists:t_products,id',
                 'rate'         => 'required|numeric|min:0',
             ]);
 
